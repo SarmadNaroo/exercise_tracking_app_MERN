adfe500bee383cd25c587a686496d80b115ee0cf
Sarmad <sarmadnaroo.bsiietkhpf20@iba-suk.edu.pk>
2024-02-25 18:14:40 +0500

    create user form and api added

--- mern_exercise_tracker/src/components/create-user.js
+++ mern_exercise_tracker/src/components/create-user.js
@@ -1,9 +1,51 @@
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
+  const [username, setUsername] = useState('');
+
+  const navigate = useNavigate();
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      username,
+    };
+    console.log(user);
+
+    axios.post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
+
+    navigate('/');
+  };
+
+
   return (
     <div>
-      <p>You are on the Create User component!</p>
+      <h3>Create New User</h3>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label>Username:</label>
+          <input 
+            type="text" 
+            className="form-control" 
+            value={username} 
+            onChange={onChangeUsername} 
+            list="userSuggestions" 
+            autoComplete="off" 
+            required 
+          />
+        </div>
+        <div className="form-group mt-3">
+          <input type="submit" value="Create User" className="btn btn-primary mr-2" />
+        </div>
+      </form>
     </div>
   );
 };
