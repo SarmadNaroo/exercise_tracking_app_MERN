a120423c7d38d0d57d7cece7849f6c3e2e147377
Sarmad <sarmadnaroo.bsiietkhpf20@iba-suk.edu.pk>
2024-02-25 18:16:12 +0500

    navbar color changed

--- mern_exercise_tracker/src/components/navbar.component.js
+++ mern_exercise_tracker/src/components/navbar.component.js
@@ -4,13 +4,13 @@ import { NavLink } from 'react-router-dom';
 
 function MyNav() {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="primary" data-bs-theme="dark">
     <Navbar.Brand as={NavLink} to="/">Exercise Tracker</Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link as={NavLink} to="/" exact>Exercise List</Nav.Link>
       <Nav.Link as={NavLink} to="/create">Create Exercise</Nav.Link>
       <Nav.Link as={NavLink} to="/user">Create User</Nav.Link>
-      <Nav.Link as={NavLink} to="/edit">Edit Exercise</Nav.Link>
+      <Nav.Link as={NavLink} to="/edit/:id">Edit Exercise</Nav.Link>
     </Nav>
   </Navbar>
   );
